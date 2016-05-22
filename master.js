3c43ae751a6e1e5dbadcd6f730c4d285bbf960eb
Aakash Goel <aakash.lpin@gmail.com>
2016-05-22 19:28:31 +0530

    move payer to data from google

--- master.js
+++ master.js
@@ -4,38 +4,29 @@
       description = document.querySelector('#description'),
       payee = document.querySelector('#payee'),
       category = document.querySelector('#category'),
-      payer = document.querySelector('#payer');
-
-  function getUrlParameterByName (name) {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
-        results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-  }
-
-  var defaultPayer = getUrlParameterByName('payer');
-
-  if (defaultPayer) {
-    payer.value = defaultPayer;
-  }
+      payer;
 
   function submitMoolah (e) {
     e.preventDefault();
-    if (!(Number(amount.value) > 0 && description.value.length)) {
-      return;
-    }
+    var userId = firebase.auth().currentUser.uid;
+    firebase.database().ref('users/' + userId).once('value').then(function(snapshot) {
+      payer = snapshot.val().displayName;
+      if (!(Number(amount.value) > 0 && description.value.length)) {
+        return;
+      }
 
-    mixpanel.track('Expense', {
-      amount: Number(amount.value),
-      description: description.value,
-      payee: payee.value,
-      category: category.options[category.selectedIndex].text,
-      payer: payer.options[payer.selectedIndex].text,
+      mixpanel.track('Expense', {
+        amount: Number(amount.value),
+        description: description.value,
+        payee: payee.value,
+        category: category.options[category.selectedIndex].text,
+        payer: payer
+      });
+
+      [amount, description, payee, category].forEach(function (item) {
+        item.value = '';
+      })
     });
-
-    [amount, description, payee, category].forEach(function (item) {
-      item.value = '';
-    })
   }
 
   form.addEventListener('submit', submitMoolah);
